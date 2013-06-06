05a83bef59d5e56a29cdea353e95e5cf68bda8da
Poturaev <nanopeni@gmail.com>
2013-06-06 12:24:01 +0700

    initial

---
+++ jquery.fixonscroll.js
@@ -0,0 +1,109 @@
+;(function($) {
+    $.fn.fixOnScroll = function(options) {
+        var b = $(this),
+
+            o = $.extend({
+                box: {top: 0, height: 0},
+                blockHeight: b.height(),
+                viewPortHeight: $(window).height()
+            }, options),
+
+            status = 'top'; // bottom, f-top, f-bottom
+
+
+        function setFixedShort() {
+            /**
+             * top -> f-top
+             * f-top -> bottom
+             *
+             * bottom -> f-bottom
+             * f-bottom -> top
+             */
+            if ('top' == status && 'f-top' != status) {
+                b.css({position: 'fixed', top: 0, bottom: 'auto'});
+                status = 'f-top';
+            }
+            else if ('bottom'== status && 'f-bottom' != status) {
+                b.css({position: 'fixed', top: 'auto', bottom: 0});
+                status = 'f-bottom';
+            }
+        }
+
+
+        function setFixedLong() {
+            /**
+             * top -> f-bottom
+             * f-bottom -> bottom
+             *
+             * bottom -> f-top
+             * f-top -> top
+             */
+
+            if ('top' == status && 'f-bottom' != status) {
+                b.css({position: 'fixed', top: 'auto', bottom: 0});
+                status = 'f-bottom';
+            }
+            else if ('bottom'== status && 'f-top' != status) {
+                b.css({position: 'fixed', top: 0, bottom: 'auto'});
+                status = 'f-top';
+            }
+        }
+
+        function setTop() {
+            if ('top' != status) {
+                b.css({position: 'relative', top: 'auto', bottom: 'auto'});
+                status = 'top';
+            }
+        }
+
+        function setBottom(top) {
+            if ('bottom' != status) {
+                b.css({position: 'relative', top: top + 'px', bottom: 'auto'});
+                status = 'top';
+            }
+        }
+
+        function scrollEvent() {
+            var box = typeof o.box == 'function' ? o.box() : o.box,
+                blockHeight = typeof o.blockHeight == 'function' ? o.blockHeight() : o.blockHeight,
+                viewPortHeight = typeof o.viewPortHeight == 'function' ? o.viewPortHeight() : o.viewPortHeight,
+                scrollTop = ($("html").scrollTop() > 0) ? $("html").scrollTop() : $("body").scrollTop(),
+                viewPort = {top: scrollTop, height: viewPortHeight, bottom: scrollTop + viewPortHeight};
+
+            box.bottom = box.top + box.height;
+            box.bottomPadded = box.bottom - blockHeight;
+            box.topPadded = box.top + blockHeight;
+
+            //console.log(box, box.bottomPadded, box.topPadded, viewPort, status);
+
+            if (viewPort.height > b.height()) {
+                if (viewPort.top < box.top) {
+                    setTop();
+                    return;
+                }
+
+                if (viewPort.top > box.bottomPadded) {
+                    setBottom(box.height - blockHeight);
+                    return;
+                }
+                setFixedShort();
+            }
+
+
+            else {
+                if (viewPort.bottom < box.topPadded) {
+                    setTop();
+                    return;
+                }
+                if (viewPort.bottom > box.bottom) {
+                    setBottom(box.height - blockHeight);
+                    return;
+                }
+                setFixedLong();
+            }
+        }
+
+        $(window).scroll(scrollEvent).resize(scrollEvent);
+        return this;
+    };
+}(jQuery));
